8178d00a4cd58286a980dafb58039f26e72e1b73
yvidjironuay <43802506+yvidjironuay@users.noreply.github.com>
2021-10-20 10:09:40 +0800

    JSON

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Welcome from './Welcome.js';
+import HotelInfo from './HotelInfo';
+
+const Main = () => {
+    return (
+        <main id="wrapper">
+            <Welcome/>
+            <HotelInfo/>
+        </main>
+    );
+}
+
+export default Main;
